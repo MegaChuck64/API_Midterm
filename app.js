c14f824618de5aa15c954848442033e618d9f283
U-MEGACHUCK64\CJ <megachuck64@gmail.com>
2018-02-13 18:12:55 -0500

    Final product, everything running on localhost:3000 or localhost:3000/index

--- app.js
+++ app.js
@@ -23,6 +23,7 @@
     -forcast.io (returns weather info using lat and lon coordinates)
 
 */
+var http = require('http');
 var path = require("path");
 var express = require("express");
 var logger = require('morgan');
@@ -48,32 +49,47 @@ var weather = new ForecastIo(options);//creates an instance of forecast.io
   Create a static route to the public folder.
   This will create a route to several essential JavaScript files and CSS files required for the app.
  */
-Your code here<----
+ 
+app.use(express.static('public'));
+
+
 /*
   Step 4 
   Create a route to the views folder. 
   This folder has all the ejs files for the app.
 */
-Your code here<----
+app.set('views', path.resolve(__dirname, 'views'));
 /*
   Step 5 
   Set Morgan in dev mode so it logs all the requests to our server.
 */
-Your code here<----
+app.use(logger('dev'))
 /*
   Step 6 
   Set your view engine to ejs.
 */
-Your code here<----
+
+app.set('view engine', 'ejs');
 
 /*
   Step 7 
   Use a get to render the index page
 */
 
-Your code here<----
+app.get("/index", 
+	function(request, response)
+	{
+		response.render("index");
+	}
+);
+app.get("/", 
+	function(request, response)
+	{
+		response.render("index");
+	}
+);
 
-//===The get below takes the 5 digets from the zip code and converts it to latitude and longitude coordinates 
+//===The get below takes the 5 digits from the zip code and converts it to latitude and longitude coordinates 
 //===It does this through an ajax request in a script in public/main.js
 //===It then takes those coordinates and passes them to forecast.io and gets the weather forecast for that region
 app.get(/^\/(\d{5})$/, function(req, res, next) {
@@ -110,7 +126,12 @@ app.use(function(req, res) {
     Step 8
     Setup the app to listen on port 3000 
 */
-Your code here<----
+http.createServer(app).listen(3000, 
+	function()
+	{
+		console.log("Server started on port 3000");
+	}
+);
 
 /*
     Step 9 
